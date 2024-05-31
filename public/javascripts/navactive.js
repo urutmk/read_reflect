938516f62e58c3462243e263c5f600aa922afb66
ujwalru <ujwalru2003@gmail.com>
2024-05-31 22:30:43 +0530

    Initial commit

---
+++ public/javascripts/navactive.js
@@ -0,0 +1,15 @@
+//script.js
+document.addEventListener('DOMContentLoaded',
+	function () {
+		const navItems = document
+			.querySelectorAll('.nav-link');
+
+		navItems.forEach(item => {
+			item.addEventListener('click',
+				function () {
+					navItems.forEach(navItem => navItem
+						.classList.remove('active'));
+					this.classList.add('active');
+				});
+		});
+	});
